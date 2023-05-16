26a1ad0f8221a4112e2708cc85f89926dcc707a8
skyglass <skyglass2001@gmail.com>
2023-05-16 23:00:40 +0200

    started implementation of production deployment pipeline for spring boot bookshop microservices

--- module-helm-addons/c8-01-keycloak-postgres-configmap.tf
+++
@@ -1,9 +0,0 @@
- # Resource: Config Map
- resource "kubernetes_config_map_v1" "keycloak_postgres_config_map" {
-   metadata {
-     name = "keycloak-postgres-dbcreation-script"
-   }
-   data = {
-    "keycloak-db.sql" = "${file("${path.module}/keycloak-db.sql")}"
-   }
- } 
